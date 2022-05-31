9dcb5e989e8b139036aa66d7bfcfbdcf76e6ab74
anhhao-123 <106544480+anhhao-123@users.noreply.github.com>
2022-05-31 10:11:52 +0700

    Add files via upload

---
+++ bai1.java
@@ -0,0 +1,18 @@
+public class bai1 {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 36;
+        int tong = a + b ;
+        System.out.println(" Tong = " + tong);
+        int hieu_1 = a - b ;
+        System.out.println(" Hieu_1 = " + hieu_1);
+        int hieu_2 = b - a;
+        System.out.println(" Hieu_2 = " + hieu_2);
+        int tich = a * b;
+        System.out.println(" Tich = " + tich);
+        double thuong_1 = (double) a/b;
+        System.out.println(" Thuong_1 = " + thuong_1);
+        double thuong_2 = (double) b/a;
+        System.out.println(" Thuong_2 = " + thuong_2);
+    }
+}
